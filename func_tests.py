60c5bda3553e83892c734521ee4b3ad4f010f8e2
alial95 <alistair_j_cooper@hotmail.com>
2020-12-08 12:03:00 +0000

    added some more tests

--- func_tests.py
+++ func_tests.py
@@ -23,6 +23,26 @@ class TestPageCount(TestCase):
 
 class TestNumItems(TestCase):
     def test_get_num_items(self):
+        with patch('requests.Response.json') as mock_request:
+            url = 'http://facebook.com'
+            mock_request.return_value = {'FHRSEstablishment': {'Header': {'ItemCount': '5'}}}
+            expected_result = 5
+            wrong_result = '5'
+            response = f.get_num_items(url)
+            self.assertEqual(response, expected_result)
+            self.assertNotEqual(response, wrong_result)
+
+class TestUrlCreator(TestCase):
+    def test_url_creator(self):
+        test_code = 43
+        expected_url = 'http://ratings.food.gov.uk/enhanced-search/en-GB/^/^/ALPHA/0/43/1/30/json'
+        self.assertEqual(f.url_creator(test_code), expected_url)
+        self.assertEqual(type(f.url_creator(test_code)), str)
+
+
+
+
+
 
 
 if __name__ == '__main__':
